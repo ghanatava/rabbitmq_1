22d2f517e31f40cf1b95406710ad7aca4057ce0a
ghanatava <ghanatva@gmail.com>
2024-03-26 11:32:14 +0530

    persistence and availability

--- recieve.py
+++ recieve.py
@@ -15,6 +15,7 @@ def callback(ch,method,properties,body):
     print("[x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue="hello",on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
